830b1fc559ef7883ea0f055f88556a681ef3d235
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-02-16 20:31:25 -0600

    Added player movement

---
+++ CSCI_370_Lab2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D player;
+    float horizontal;
+    float vertical;
+    public float runSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        player.linearVelocity = new Vector2(horizontal*runSpeed, vertical*runSpeed);   
+    }
+}
