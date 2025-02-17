bccb0336011f3ed5095fc1f7070126e4264cca8a
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-02-16 22:46:04 -0600

    Added code for basic dialogue implementation

--- CSCI_370_Lab2/Assets/Scripts/GameManager.cs
+++ CSCI_370_Lab2/Assets/Scripts/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,6 +9,14 @@ public class GameManager : MonoBehaviour
     public static GameManager main;
     public Transform startpoint;
     public Transform[] path;
+    [SerializeField] TextMeshProUGUI dialogueText;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] GameObject dialoguePanel;
+    public static event Action OnDialogueStarted;
+    public static event Action OnDialogueEnded;
+    bool skipLineTriggered;
+    public float charactersPerSec = 90;
+
     private void Awake() {
         main = this;
     }
@@ -20,4 +31,54 @@ public class GameManager : MonoBehaviour
     {
         
     }
+    public void StartDialogue(String[] dialogue, int StartPosition, string name) {
+        nameText.text = name + "...";
+        dialoguePanel.SetActive(true);
+        StopAllCoroutines();
+        StartCoroutine(RunDialogue(dialogue, StartPosition));
+    }
+
+    IEnumerator RunDialogue(string[] dialogue, int StartPosition) {
+        skipLineTriggered = false;
+        OnDialogueStarted?.Invoke();
+
+        for (int i = StartPosition; i < dialogue.Length; i++) {
+            dialogueText.text = null;
+            StartCoroutine(TypeTextUncapped(dialogue[i]));
+
+            while (!skipLineTriggered) {
+                yield return null;
+            }
+            skipLineTriggered = false;
+        }
+
+        OnDialogueEnded?.Invoke();
+        dialoguePanel.SetActive(false);
+    }
+
+    public void SkipLine(){
+        skipLineTriggered = true;
+    }
+
+    IEnumerator TypeTextUncapped(string text) {
+        float timer = 0;
+        float interval = 1 / charactersPerSec;
+        string textBuffer = null;
+        char[] chars = text.ToCharArray();
+        int i = 0;
+
+        while (i < chars.Length) {
+            if (timer < Time.deltaTime) {
+                textBuffer += chars[i];
+                dialogueText.text = textBuffer;
+                timer += interval;
+                i++;
+            }
+            else{
+                timer -= Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+
 }
